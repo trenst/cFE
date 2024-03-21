77acad9947cc31ccf0ca9d0a67c23a494383c68e
Steve Kadlec <steven.kadlec@ga.com>
2024-03-21 14:28:52 -0600

    Add remap filter for sbn

--- cmake/mission_defaults.cmake
+++ cmake/mission_defaults.cmake
@@ -56,6 +56,7 @@ set(MISSION_GLOBAL_STATIC_APPLIST)
 set(MISSION_MODULE_SEARCH_PATH
     "apps"                  # general purpose $[top}/apps directory
     "apps/sbn/modules/protocol"
+    "apps/sbn/modules/filter"
     "libs"                  # general purpose $[top}/libs directory
     "psp/fsw/modules"       # modules for optional platform abstraction, associated with PSP
     "cfe/modules"           # modules for optional core functions, associated with CFE
